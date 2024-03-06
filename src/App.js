2cc690b1a6265b272047c816275130fd969300a2
quachvanvu <vuq41783@gmail.com>
2024-03-06 13:58:43 +0700

    2

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/admin" element={<admin />} />
         </Routes>
       </BrowserRouter>
     </div>
