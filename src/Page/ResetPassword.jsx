1f37e736bc14be31fcc178dda6781a7fd8e49ea0
ahntu14 <phamtu559@gmail.com>
2024-04-29 17:45:16 +0700

    a

--- src/Page/ResetPassword.jsx
+++ src/Page/ResetPassword.jsx
@@ -1,16 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import './CSS/ResetPassword.css';
+import { Button, Container, TextField, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import CheckIcon from '@mui/icons-material/Check';
 
 const ResetPassword = () => {
     const [password, setPassword] = useState('');
     const [rePassword, setRePassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
-    const [success, setSuccess] = useState(false);
+    const [showRepassword, setShowRepassword] = useState(false);
     const [token, setToken] = useState('');
+    const [show, setShow] = useState(false);
+    const [response, setResponse] = useState(false);
     const navigate = useNavigate();
+
     useEffect(() => {
         const queryString = window.location.search;
         const token = queryString.substring(queryString.indexOf('token=') + 6);
@@ -27,47 +33,137 @@ const ResetPassword = () => {
                     newPassword: rePassword,
                 });
                 if (response.data.status === true) {
+                    setResponse(true);
                     setPassword('');
                     setRePassword('');
-                    setSuccess(true);
                     toast.success('Thay đổi mật khẩu thành công');
                 }
             } catch (error) {
+                setResponse(false);
                 setErrorMessage('Đã xảy ra lỗi');
             }
         }
     };
 
     return (
-        <div className="signup">
-            <div className="signup-container">
-                <h2>Tạo lại mật khẩu</h2>
-                <div className="signup-form">
-                    <input
-                        type="text"
+        <Container
+            maxWidth="sm"
+            sx={{
+                marginTop: '150px',
+                border: '1px solid lightgray',
+                borderRadius: '10px',
+                height: '300px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            {response === true ? (
+                <>
+                    <>
+                        <Typography sx={{ textAlign: 'center' }}>Thành công</Typography>
+                        <CheckIcon
+                            sx={{
+                                marginTop: '25px',
+                                marginBottom: '25px',
+                                color: 'blue',
+                                fontSize: '50px',
+                            }}
+                        />
+                        <Button onClick={() => navigate('/login')} variant="outlined">
+                            Quay về đăng nhập
+                        </Button>
+                    </>
+                </>
+            ) : (
+                <>
+                    <Typography sx={{ textAlign: 'center', fontSize: '18px' }}>Tạo lại mật khẩu</Typography>
+                    <TextField
+                        size="small"
+                        type={show ? 'text' : 'password'}
                         placeholder="Mật khẩu mới"
-                        value={password}
+                        fullWidth
+                        sx={{ margin: '15px 0px', fontSize: '15px', position: 'relative' }}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    <input
-                        type="text"
-                        placeholder="Xác nhận mật khẩu mới"
-                        value={rePassword}
+                    {show ? (
+                        <VisibilityOffIcon
+                            sx={{
+                                position: 'absolute',
+                                marginBottom: '15px',
+                                marginTop: '-75px',
+                                marginLeft: '500px',
+                            }}
+                            onClick={() => setShow(!show)}
+                        />
+                    ) : (
+                        <VisibilityIcon
+                            onClick={() => setShow(!show)}
+                            sx={{
+                                position: 'absolute',
+                                marginBottom: '15px',
+                                marginTop: '-75px',
+                                marginLeft: '500px',
+                            }}
+                        />
+                    )}
+
+                    <TextField
+                        size="small"
+                        type={showRepassword ? 'text' : 'password'}
+                        placeholder="Nhập lại mật khẩu mới"
+                        fullWidth
+                        sx={{
+                            fontSize: '15px',
+                            marginBottom: '15px',
+                            position: 'relative',
+                        }}
                         onChange={(e) => setRePassword(e.target.value)}
                     />
-                </div>
-                {errorMessage && <p className="error-message">{errorMessage}</p>}
-                {/* <button onClick={handleChangePassword}>Đăng ký</button>
-                <button onClick={handleChangePassword}>Đăng ký</button> */}
-                {success ? (
-                    <button style={{ backgroundColor: 'green' }} onClick={() => navigate('/login')}>
-                        Quay lại đăng nhập
-                    </button>
-                ) : (
-                    <button onClick={handleChangePassword}>Thay đổi mật khẩu</button>
-                )}
-            </div>
-        </div>
+                    {showRepassword ? (
+                        <VisibilityOffIcon
+                            sx={{
+                                position: 'absolute',
+                                marginTop: '25px',
+                                marginLeft: '500px',
+                            }}
+                            onClick={() => setShowRepassword(!showRepassword)}
+                        />
+                    ) : (
+                        <VisibilityIcon
+                            onClick={() => setShowRepassword(!showRepassword)}
+                            sx={{
+                                position: 'absolute',
+                                marginTop: '25px',
+                                marginLeft: '500px',
+                            }}
+                        />
+                    )}
+                    <div style={{ position: 'relative', width: '100%' }}>
+                        {errorMessage && (
+                            <p
+                                style={{
+                                    fontSize: '14px',
+                                    color: 'red',
+                                    top: '-15px',
+                                    position: 'absolute',
+                                    bottom: '5px',
+                                    left: 0,
+                                    right: 0,
+                                    textAlign: 'center',
+                                }}
+                            >
+                                {errorMessage}
+                            </p>
+                        )}
+                    </div>
+                    <Button variant="outlined" sx={{ marginTop: '25px' }} onClick={handleChangePassword}>
+                        Tạo lại mật khẩu
+                    </Button>
+                </>
+            )}
+        </Container>
     );
 };
 
